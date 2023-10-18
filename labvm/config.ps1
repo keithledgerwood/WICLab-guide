edc70d1ee829b3f0226c5cab13fc159986f828b4
Keith Ledgerwood <keithledgerwood@gmail.com>
2023-10-18 16:00:05 -0500

    Powershell Updates

--- labvm/config.ps1
+++ labvm/config.ps1
@@ -49,7 +49,6 @@ Remove-Item -Path "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pi
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Force -Recurse 
 Stop-Process -ProcessName explorer -Force
 Start-process -WindowStyle hidden explorer 
- 
 
 #Replace Chrome Preferences file
 iwr -uri https://raw.githubusercontent.com/keithledgerwood/WICLab-guide/dev/labvm/Preferences -OutFile .\Preferences  
@@ -61,6 +60,11 @@ copy-item .\Bookmarks -Destination "C:\Users\Administrator\AppData\Local\Google\
 
 #Downlaod Okta Verify and place in downloads folder
 iwr -uri https://okta.okta.com/api/v1/artifacts/WINDOWS_OKTA_VERIFY/download?releaseChannel=GA -OutFile .\Downloads\OktaVerifySetup  
+
+$a = (New-Object -comObject Shell.Application).Windows() |
+ ? { $_.FullName -ne $null} |
+ ? { $_.FullName.toLower().Endswith('\explorer.exe') }
+ $a | % { $_.Quit() }
 
 exit
 
