066cc806cb9e6540a02e66b87f60a92ef2f95a49
jordangoodyear <146869359+jordangoodyear@users.noreply.github.com>
2023-10-18 15:35:29 -0500

    Update config.ps1
    updated to only chrome with no page
    added okta verify setup to download to download folder

--- labvm/config.ps1
+++ labvm/config.ps1
@@ -4,11 +4,10 @@ Remove-Item C:\Users\*\Desktop\*lnk -Force
 Remove-Item C:\Users\*\Desktop\Microsoft -Recurse
 Remove-Item C:\Users\*\Desktop\logs -Recurse
 
-#Add Chrome Shortcut and launch as O365
+#Add Chrome Shortcut, blank
 $shell = New-Object -comObject WScript.Shell
-$shortcut = $shell.CreateShortcut("$Home\Desktop\Launch Office 365.lnk")
+$shortcut = $shell.CreateShortcut("$Home\Desktop\Launch Chrome.lnk")
 $shortcut.TargetPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-$shortcut.Arguments = "https://admin.microsoft.com"
 $shortcut.Save()
 
 #Shortcut to Flowpack
@@ -18,22 +17,17 @@ $shortcut.TargetPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $shortcut.Arguments = "https://raw.githubusercontent.com/keithledgerwood/WICLab-guide/dev/workflows/titleChanged.flow"
 $shortcut.Save()
 
-
-#Add Chrome Shortcut to Okta Demo
-$shell = New-Object -comObject WScript.Shell
-$shortcut = $shell.CreateShortcut("$Home\Desktop\Launch Lab Guide.lnk")
-$shortcut.TargetPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-$shortcut.Arguments = "http://labs.demo.okta.com"
-$shortcut.Save()
-
 #Shortcut for Notepad on Desktop
  $shell = New-Object -comObject WScript.Shell 
  $shortcut = $shell.CreateShortcut("$Home\Desktop\Notepad.lnk") 
  $shortcut.TargetPath = "%windir%\system32\notepad.exe" 
  $shortcut.Save()
  
-#Add download folder to desktop
-#New-Item "$Home\Desktop\Download" -ItemType Directory
+#Add download shortcut to desktop
+ $shell = New-Object -comObject WScript.Shell 
+ $shortcut = $shell.CreateShortcut("$Home\Desktop\Downloads.lnk") 
+ $shortcut.TargetPath = "C:\Users\Administrator\Downloads" 
+ $shortcut.Save()
 
 #Remove pinned icons from taskbar
 Remove-Item -Path "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*" -Force -Recurse 
@@ -49,6 +43,9 @@ copy-item .\Preferences -Destination "C:\Users\Administrator\AppData\Local\Googl
 #Replace Chrome Bookmarks file
 iwr -uri https://raw.githubusercontent.com/keithledgerwood/WICLab-guide/dev/labvm/Bookmarks -OutFile .\Bookmarks  
 copy-item .\Bookmarks -Destination "C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default" -recurse -Force 
+
+#Downlaod Okta Vertify and place in downloads folder
+iwr -uri https://okta.okta.com/api/v1/artifacts/WINDOWS_OKTA_VERIFY/download?releaseChannel=GA -OutFile .\Downloads\OktaVerifySetup  
 
 exit
 
